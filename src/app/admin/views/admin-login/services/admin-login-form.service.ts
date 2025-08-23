a8777daa63bd20337f110e851ab9c1a82c812c1c
Gustavo Solano <gustavo.esp@icloud.com>
2025-08-23 14:10:26 -0600

    feat: addede login signal on update

---
+++ src/app/admin/views/admin-login/services/admin-login-form.service.ts
@@ -0,0 +1,19 @@
+import { computed, Injectable, Signal, signal, WritableSignal } from '@angular/core';
+import { login } from '../../../../shared/domain';
+import { Login } from '../../../../shared/models/banner.constant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminLoginFormService {
+
+  private login: WritableSignal<login> = signal<login>(structuredClone(Login));
+
+  public loginSignal: Signal<login> = computed(() => this.login());
+
+  constructor() { }
+
+  onUpdate(login: login): void {
+    this.login.set(login);
+  }
+}
---
+++ src/app/shared/models/banner.constant.ts
@@ -0,0 +1,6 @@
+import { login } from "../domain";
+
+export const Login: login = {
+  username: '',
+  password: ''
+}
